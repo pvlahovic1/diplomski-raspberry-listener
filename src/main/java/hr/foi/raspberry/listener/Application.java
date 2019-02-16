84b9266b41e086eedd0cf3bca109ef709b87e150
Pavao Vlahovic <pvlahovic@foi.hr>
2019-02-16 19:34:42 +0100

    Initial application

---
+++ src/main/java/hr/foi/raspberry/listener/Application.java
@@ -0,0 +1,14 @@
+package hr.foi.raspberry.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
